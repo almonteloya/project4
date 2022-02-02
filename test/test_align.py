5560d8b4168847a7d94efd4fcfb4fac6f154784e
almonteloya <ana.almonteloya@gmail.com>
2022-02-02 14:25:00 -0800

    comment test

--- test/test_align.py
+++ test/test_align.py
@@ -14,8 +14,22 @@ def test_nw_alignment():
     """
     seq1, _ = read_fasta("./data/test_seq1.fa")
     seq2, _ = read_fasta("./data/test_seq2.fa")
-    print(seq1,seq2)
-    pass
+    
+    test = NeedlemanWunsch("./substitution_matrices/BLOSUM62.mat",-10,-1)
+    test.align("MQR", "MYQR")                     
+                   
+
+    ## Calculate this matrices manually
+    ## Looking only at the first two rows
+    manual_alignmatrix = np.matrix([[0,-(np.inf),-(np.inf),-(np.inf),-(np.inf)], [-(np.inf), 5,-12,-12,-14]])
+    manual_gapA = np.matrix([[-10,-(np.inf),-(np.inf),-(np.inf),-(np.inf)], [-(11), -22,-23,-24,-25]])  
+    manual_gapB = np.matrix([[-10,-11,-12,-13,-14], [-(np.inf), -22,-6,-7,-8]])  
+
+    assert(np.all(test._align_matrix[:2] == manual_alignmatrix))
+    assert(np.all(test._gapA_matrix[:2] == manual_gapA))
+    assert(np.all(test._gapB_matrix[:2] == manual_gapB))
+
+
 
 def test_nw_backtrace():
     """
@@ -27,9 +41,10 @@ def test_nw_backtrace():
     """
     seq3, _ = read_fasta("./data/test_seq3.fa")
     seq4, _ = read_fasta("./data/test_seq4.fa")
-    pass
+    print(seq3,seq4)
 
 
 
 
-def test_nw_alignment():
+test_nw_backtrace()
+test_nw_alignment()
